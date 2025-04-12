3113747c8e6970ab3f3be02b4d886c68d470ecf0
Anjola <sheyiakinso@gmail.com>
2025-04-12 18:16:33 +0100

    welcome-board

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import logo from './assets/Frame 13.png'
 import './App.css'
+
 import Navbar from './components/navbar'
 import Sidebar from './components/sidebar'
 import Board from './components/board'
@@ -10,13 +9,13 @@ function App() {
 
   return (
     <>
-      <div>
+      
         <Navbar/>
         <div className="dashboard">
           <Sidebar/>
           <Board/>
         </div>
-      </div>
+
     </>
   )
 }
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,11 +1,30 @@
-import { Container } from "@mui/material";
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import FolderIcon from '@mui/icons-material/Folder';
+
 import React from "react";
 const Board = () => {
     return ( 
         <div className="board">
             <div className="displayBoard">
-
+                <div className="welcomeHeader">
+                    <div className="welcomeText">
+                        <h3>Welcome, Moyosore Okanlawon</h3>
+                        <p>Open your files or folders here!</p>
+                    </div>
+                    <button>
+                        <AddRoundedIcon style={{
+                            paddingRight: 5
+                        }}/>
+                        Create
+                    </button>
+                </div>
+                <div className="actionBox">
+                    <div className="groupContainer">
+                        
+                    </div>
+                </div>
             </div>
+
             <div className="activityBoard"></div>
         </div>
      );
