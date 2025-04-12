21edbea9b6d3d8f0cc967f38dcb8271995f8183c
Anjola <sheyiakinso@gmail.com>
2025-04-12 15:35:37 +0100

    navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import PublicIcon from '@mui/icons-material/Public';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import profileImg from '../assets/profileImage.jpg'
+import logo from '../assets/Frame 13.png';
+
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <div className="logo">
+                <img src={logo} alt="" />
+                <h3>The Hub</h3>
+            </div>
+            <div className="searchbar">
+                <SearchOutlinedIcon style={{ fontSize: 20, color: 'white', paddingLeft: 10}}/>
+                <input type="search" name="" id="" placeholder='Search for your files and groups'/>
+            </div>
+            <div className="profile">
+                <PublicIcon style={{ fontSize: 30, color: 'white'}}/>
+                <HelpOutlineOutlinedIcon style={{ fontSize: 30, color: 'white'}}/>
+                <img src={profileImg} alt="" />
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
