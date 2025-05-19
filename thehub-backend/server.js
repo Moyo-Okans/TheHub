d273b28abf8a53b639e6b556595a0bec75a9fefc
Ololade131 <olukayodelolade@gmail.com>
2025-05-19 11:46:32 +0100

    lol

--- thehub-backend/server.js
+++ thehub-backend/server.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const userRoutes = require("./routes/userRoutes");
 const app = express();
 const PORT = 3000;
-
 // Middlewares
 app.use(cors());
 app.use(express.json());
-
 // Routes
+app.use('/api/users', userRoutes);
 app.get('/api/hello', (req, res) => {
     res.json({ message: 'Hello from backend!' });
 });
-  
 app.get("/", (req, res) => {
   res.send("Welcome to The Hub Backend!");
 });
-
-
 const mongoURI =
   "mongodb+srv://olukayodelolade:05TheHub2020@moyoshub.md6ilwj.mongodb.net/?retryWrites=true&w=majority&appName=Moyoshub";
 mongoose
@@ -26,9 +22,6 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 // Start the server
-
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
