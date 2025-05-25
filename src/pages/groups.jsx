b1fd7443e0d573a896203f03610b3ddba6cb749a
Excellence Jephter <listenexcellence@gmail.com>
2025-05-25 19:12:27 +0100

    Responsiveness and Dropdown

--- src/pages/groups.jsx
+++ src/pages/groups.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import Navbar from "../components/navbar";
 import Sidebar from "../components/sidebar";
 import '../style/groups.css'
 import emptyState from '../assets/empty_state.png'
-import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import { StarBorderOutlined } from "@mui/icons-material";
 import ScheduleRoundedIcon from '@mui/icons-material/ScheduleRounded';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -12,6 +13,31 @@ import FolderIcon from '@mui/icons-material/Folder';
 
 
 function Groups() {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const dropdownRef = useRef(null);
+  
+    // Toggle dropdown visibility
+    const toggleDropdown = () => {
+      setIsDropdownOpen(!isDropdownOpen);
+    };
+  
+    // Close dropdown when clicking outside
+    useEffect(() => {
+      const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+          setIsDropdownOpen(false);
+        }
+      };
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, []);
+  
+    const handleFileChange = () => {
+      console.log();
+    }
+
   return (
     <>
       <div className="dashboard">
@@ -22,10 +48,39 @@ function Groups() {
             <div className="bodyContainer">
               <div className="groupHeader">
               <h1>Groups</h1>
-              <button>
-                  <AddRoundedIcon />
-                  Create
+               <div className="dropdown" ref={dropdownRef}>
+          <button onClick={toggleDropdown} className="create-button">
+            <AddRoundedIcon />
+            Create
+            <KeyboardArrowDownIcon sx={{ fontSize: "16px" }} />
+          </button>
+          {isDropdownOpen && (
+            <ul className="dropdown-menu">
+              <li>
+                <button onClick={() => console.log("Create Group")}>
+                  Create Group
                 </button>
+              </li>
+              <li>
+                <label htmlFor="input-file">
+                  <input
+                    type="file"
+                    accept="image/*"
+                    id="input-file"
+                    multiple
+                    hidden
+                    style={{ display: "none" }}
+                    onChange={handleFileChange}
+                  />
+                  <div className="uploadButton">
+                    <h4>Upload File</h4>
+                  </div>
+                </label>
+
+              </li>
+            </ul>
+          )}
+        </div>
               </div>
               <div className="groups">
                 <div className="tags">
