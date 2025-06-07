c978bb4ee226e885cc87f8deeb7b37f2ae1d0db4
Moyo Okans <moyookanlawon@gmail.com>
2025-06-07 15:37:36 +0100

    deploy again

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://thehub-backend.onrender.com',
+  baseURL: 'https://thehub-backend.onrender.com/api',
 });
 
 export default api;
